2b285a8d381a391fd51fb3fcf2f42a581c68b427
bombk <bomncit@gmail.com>
2025-01-31 12:19:11 +0545

    allowed host

--- bomlog/settings.py
+++ bomlog/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['api.bombk.com.np']
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
